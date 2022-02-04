7b729bf096135dc2e284e73c4df725239dc2e175
Erpen <Erpen@BIMERPEN>
2022-02-04 15:24:48 +0300

    Add project files.

---
+++ Business/Abstract/ICartService.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using Entities.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+   public interface ICartService
+    {
+        void AddToCart(Cart cart, Product product);
+        void RemoveToCart(Cart cart, int productId);
+        List<CartLine> CartLines(Cart cart);
+        
+    }
+}
---
+++ MvcWebUI/ViewComponents/CategoryListViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using MvcWebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcWebUI.ViewComponents
+{
+    public class CategoryListViewComponent : ViewComponent
+    {
+        //VİEWCOMPONENT'İ LAYOUTDA ÇAĞIRICAZ ÇÜNKÜ COMPONENT BİR SAYFADA İKİ FARKLI COMPONENT KULLANABİLİRİZ CATEGORY SABİT GELİCEK AYNI LAYOUT GİBİ
+        private ICategoryService _categoryService;
+
+        public CategoryListViewComponent(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        //VİEW'İ DEFAULT İSMİNDE OLMASI GEREKİYOR
+        public ViewViewComponentResult Invoke()
+        {
+            var model = new CategoryListViewModel
+            {
+                Categories = _categoryService.GetAll(),
+                CurrentCategory=Convert.ToInt32(HttpContext.Request.Query["category"])
+            };
+            return View(model);
+        }
+    }
+}   
